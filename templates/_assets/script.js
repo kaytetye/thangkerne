21f614930f049aa77102a57b60d5c7729fa19946
ben foley <ben@cbmm.io>
2021-12-29 18:19:42 +1000

    change menu style

--- templates/_assets/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-  $(".entryAudio" ).click(function() {
-    var url = $(this).data("file");
-    $("#wsumarcs-dummycat").html('<audio controls autoplay hidden><source src="' + url + '" type="audio/mpeg"></audio>');
-  });
-});
