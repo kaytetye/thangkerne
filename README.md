2a1933a26c9ecebc33ce379f7fb40ef1ffb851bb
ben foley <ben@cbmm.io>
2021-12-30 13:51:37 +1000

    index styles

--- README.md
+++ README.md
@@ -2,10 +2,30 @@
 
 The bird app content, remade as a website. 
 
+
+## TODO
+- add about page
+
+
+## Build
+
+1. Flatten media with the `prepare_media.py` script. This will get audio and images out of the media dir and copy into the output dir.
+2. Build the HTML pages with `build_html.py` script. It will read the CSV files in the `content` dir and create an index page, and entry pages for each published entry in the data.
+
+
+## Deploy
+
+Login to Firebase with `kaytetye.code` account
+
+```
+firebase deploy --only hosting
+```
+
+
+## Notes
+
 The content in the following folders is handled by `git-lfs`. 
+
 ```
 git lfs track 'media/**' 'output/audio/**' 'output/images/**'
 ```
-
-# TODO
-- include png in media processing script
--- scripts/build_html.py
+++ scripts/build_html.py
@@ -26,7 +26,6 @@ def get_categories_entries():
     # Read all category-entry pairs as a list, also flip the col order
     print("==== Getting category entries")
     categories_entries = {}
-    entries_categories = {}
     with Path("../content/jila-kaytetye-admin/categories_entries.csv").open("r") as categories_entries_file:
         csv_reader = csv.reader(categories_entries_file)
         next(csv_reader)  # skip the header
@@ -37,11 +36,7 @@ def get_categories_entries():
                 categories_entries[category_id].append(entry_id)
             else:
                 categories_entries[category_id] = [entry_id]
-            if entry_id in entries_categories:
-                entries_categories[entry_id].append(category_id)
-            else:
-                entries_categories[entry_id] = [category_id]
-    return categories_entries, entries_categories
+    return categories_entries
 
 
 def get_entries():
@@ -83,17 +78,18 @@ def get_entries():
         for row in csv_reader:
             id = int(row["Id"])
             # Add row data to entry array here
-            entries[id] = {"id": id,
-                           "word": row["Entry word"],
-                           "translation": row["Translation"],
-                           "published": row["Published?"],
-                           "image_file_name": slugify(row["Image file name"]),
-                           "audio_file_name": slugify((row["Audio file name"])),
-                           "sentence": row["Sentence"],
-                           "sentence_translation": row["Sentence translation"],
-                           "scientific_name": row["Scientific name"],
-                           "call_audio_file_name": slugify(row["Call audio file name"]),
-                           }
+            if row["Published?"] == "true":
+                entries[id] = {"id": id,
+                               "word": row["Entry word"],
+                               "translation": row["Translation"],
+                               "published": row["Published?"],
+                               "image_file_name": slugify(row["Image file name"]),
+                               "audio_file_name": slugify((row["Audio file name"])),
+                               "sentence": row["Sentence"],
+                               "sentence_translation": row["Sentence translation"],
+                               "scientific_name": row["Scientific name"],
+                               "call_audio_file_name": slugify(row["Call audio file name"]),
+                               }
     return entries
 
 
@@ -105,11 +101,6 @@ def build_index_page(entries: Dict, project_output_path: Path):
     :param project_output_path:
     :return:
     """
-    # copy assets
-    shutil.copytree("../templates/_assets",
-                    project_output_path.joinpath("_assets"), dirs_exist_ok=True)
-    print(entries)
-
     with open("../templates/index.html") as template_file:
         tm = Template(template_file.read())
         html = tm.render(entries=entries)
@@ -134,6 +125,7 @@ def build_entry_pages(entries: Dict,
     # copy assets
     shutil.copytree("../templates/_assets",
                     project_output_path.joinpath("_assets"), dirs_exist_ok=True)
+    shutil.copy("../templates/favicon.ico", project_output_path)
 
     with open("../templates/entry.html") as template_file:
         tm = Template(template_file.read())
@@ -156,7 +148,7 @@ def main():
     reset_path(entries_output_path)
 
     categories = get_categories()
-    categories_entries, entries_categories = get_categories_entries()
+    categories_entries = get_categories_entries()
     entries = get_entries()
 
     menu = []
@@ -164,12 +156,15 @@ def main():
         if category["id"] in categories_entries:
             sub_menu = {"name": category["name"], "entries": []}
             for entry_id in categories_entries[category["id"]]:
-                entry = entries[entry_id]
-                sub_menu["entries"].append({
-                    "id": entry_id,
-                    "text": entry["word"],
-                    "slug": f'{entry["word"]}.html'
-                })
+                # Entries might not contain this id if the entry is not published
+                if entry_id in entries:
+                    entry = entries[entry_id]
+                    sub_menu["entries"].append({
+                        "id": entry_id,
+                        "text": entry["word"],
+                        "slug": f'{entry["word"]}.html',
+                        "image_file_name": entry["image_file_name"]
+                    })
             custom_sort(sub_menu["entries"], "text")
             menu.append(sub_menu)
 
